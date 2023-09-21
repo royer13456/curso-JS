5e5a06e527443176d4c2888236257079b94ed0df
royer <i2226824@continental.edu.pe>
2023-09-20 21:52:07 -0500

    curso JS

---
+++ app.js
@@ -0,0 +1,10 @@
+let password = "123";
+let input = "123";
+
+let result = input == password;
+
+if(result === true){
+   console.log("login correcto");
+} else {
+   console.log("contraseña incorrecta");
+}
